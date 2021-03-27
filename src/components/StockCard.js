fa8ee5d835ad439a7a095bb913926d0b5cf53de5
Jeffrey Shen <jeffreyshen13@gmail.com>
2021-03-26 19:38:28 -0700

    clean up space for stocks

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -4,6 +4,12 @@ import { Link } from 'gatsby'
 import Image from './Image'
 import './PostCard.css'
 
+
+function getColorFromChange(change) {
+
+}
+
+
 const NewsPostCard = ({
   company,
   price,
@@ -12,11 +18,8 @@ const NewsPostCard = ({
   className = '',
   ...props
 }) => (
-    <div className="PostCard--Content">
+    <div>
       {company && <h3 className="PostCard--Title">{company}</h3>}
-      <div className="PostCard--Category">
-        {categories && categories.map(cat => cat.category).join(', ')}
-      </div>
       {parseFloat(change) > 0 && price && <div className="PostCard--Price-g">${parseFloat(price).toFixed(2)} (+{parseFloat(change).toFixed(2)}%)</div>}
       {parseFloat(change) === 0 && price && <div className="PostCard--Price-def">${parseFloat(price).toFixed(2)} ({parseFloat(change).toFixed(2)}%)</div>}
       {parseFloat(change) < 0 && price && <div className="PostCard--Price-r">${parseFloat(price).toFixed(2)} ({parseFloat(change).toFixed(2)}%)</div>}
